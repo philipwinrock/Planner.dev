b87c2f848e862de3e961d40510c8e2e87fcd7d59
philip james winrock <philipjameswinrock@philips-MacBook-Pro.local>
2014-10-02 13:18:04 -0500

    Clean code and organize

--- public/inc/address_data_store.php
+++ public/inc/address_data_store.php
@@ -1,30 +1,25 @@
 <?php
 require_once('filestore.php');
-class AddressDataStore extends Filestore{
+class AddressDataStore extends filestore    {
 
     //public $filename = '../data/address_book.csv';
 
-   public function __construct($filename = '') {
+    public function __construct($filename = '') {
 
-    parent::__construct(strtolower($filename));
+        parent::__construct(strtolower($filename));
 
-   }
+    }
    
-
+    public function readaddressBook()   {
     
-    public function readaddressBook()
-    {
         // Code to read file $this->filename
         // CREATE EMPTY $addressBook
         $addressBook = $this->read();
         return $addressBook;
     }
         
-
-      
-
-    public function writeaddressBook($addressArray)
-    {
+public function writeaddressBook($addressArray){
+    
        $this->write($addressArray);
     }
 
